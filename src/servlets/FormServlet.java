274c39f1bf53aede249e51806b2f67919173e851
hibernaut <gregory.petrenko@gmail.com>
2022-11-03 15:16:23 +0200

    Add servlet examples for processing form parameters

--- src/servlets/FormServlet.java
+++ src/servlets/FormServlet.java
@@ -1,9 +1,7 @@
 package servlets;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
