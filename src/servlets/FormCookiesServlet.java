6175346a500d0d52053d91eb5bbf20d2ea5c043a
hibernaut <gregory.petrenko@gmail.com>
2022-11-07 17:40:01 +0200

    Add servlet examples working with cookie files

---
+++ src/servlets/FormCookiesServlet.java
@@ -0,0 +1,50 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FormCookiesServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        Cookie firstName = new Cookie("first_name", request.getParameter("first_name"));
+        Cookie lastName = new Cookie("last_name", request.getParameter("last_name"));
+
+        firstName.setMaxAge(60 * 60 * 24);
+        lastName.setMaxAge(60 * 60 * 24);
+
+        response.addCookie(firstName);
+        response.addCookie(lastName);
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        String title = "Setting Cookie Example";
+        String docType = "<!DOCTYPE html>\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor = \"#f0f0f0\">\n" +
+                "<h1 align = \"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                "  <li><b>First Name</b>: "
+                + request.getParameter("first_name") + "\n" +
+                "  <li><b>Last Name</b>: "
+                + request.getParameter("last_name") + "\n" +
+                "</ul>\n" +
+                "<p>Cookie files successfully saved!</p>\n" +
+                "</body>" +
+                "</html>"
+        );
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
---
+++ src/servlets/ReadCookiesServlet.java
@@ -0,0 +1,43 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ReadCookiesServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        Cookie cookie = null;
+        Cookie[] cookies = null;
+
+        cookies = request.getCookies();
+
+        PrintWriter out = response.getWriter();
+        String title = "Reading Cookies Example";
+        String docType = "<!DOCTYPE html>\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor = \"#f0f0f0\">\n" );
+
+        if (cookies != null) {
+            out.println("<h2> Found Cookies Name and Value</h2>");
+
+            for (Cookie value : cookies) {
+                cookie = value;
+                out.print("Name : " + cookie.getName() + ",  ");
+                out.print("Value: " + cookie.getValue() + " <br/>");
+            }
+        } else {
+            out.println("<h2>No cookies founds</h2>");
+        }
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
